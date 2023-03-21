f4429df5028ec977dfb476482b5c190b8f1cf4c3
ionutbila <ionutbila@ymail.com>
2023-03-21 17:30:39 +0200

    am creat un al doilea program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Hi there!");
+		System.out.println("Hi everybody!");
+		System.out.println("Hi there!");
+		System.out.println("Hi everybody!");
+	}
+}
